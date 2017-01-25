e2341a4f4aa54f5abef6349406635bbe6114481c
Daniel Souza da Silva <dssilva@padtec.com.br>
2017-01-25 10:01:23 -0200

    Third class
    Change-Id: I89faaab717da835425abcace1802d9fe38da1b7b

--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.models;
 
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,7 +17,9 @@ public class Produto {
     private String titulo;
     private String descricao;
     private int paginas;
-
+    
+    @ElementCollection
+    private List<Preco> precos;
 
     public String getTitulo() {
         return titulo;
@@ -34,8 +39,13 @@ public class Produto {
     public void setPaginas(int paginas) {
         this.paginas = paginas;
     }
-
-    @Override
+    public List<Preco> getPrecos() {
+		return precos;
+	}
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
+	}
+	@Override
     public String toString() {
         return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
     }
